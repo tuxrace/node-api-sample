7a6891b8d3fb1e6ea6b853901c6409957bf95876
Gerard Banasig <gerardb@istackmanila.com>
2017-08-13 17:37:01 +0800

    add middleware verify

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const router = express.Router()
-
-// app.use(cors())
-app.use('/', router)
 const key = '3h9xgo4w'
+
+function verify(req, res, next){
+  console.log('inn')
+  if (req.query.key === '3h9xgo4w')
+    next()
+  res.status(400).send('not authorized')
+}
+app.use(verify)
+//app.use(cors())
+app.use('/', router)
 
 router.options('*', cors()) 
 
